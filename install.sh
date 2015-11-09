353c0e781f500ebcf60ae3eba2bebc847c21586d
Séв(aѕтιen) L <dzogrim@users.noreply.github.com>
2015-11-09 18:52:06 +0100

    Update install.sh
    Add GNU version of echo and stat…

--- install.sh
+++ install.sh
@@ -5,12 +5,22 @@
 # and then hooking them to the login and logout
 #
 
+#-- Find 'echo' program in PATH or use Mac Ports default location
+ECHO="$( which echo )"
+ECHO="${ECHO:-/opt/local/libexec/gnubin/echo}"
+
+#-- Bash version check for 'echo'
+[ -n "${BASH_VERSION}" ] \
+  && ECHO="echo -e" \
+  || ECHO="echo"
+
 if [ "$(id -u)" != "0" ]; then
-	echo "You need administrative privileges to install this script.\nPlease run\nsudo sh install.sh"
+	${ECHO} "You need administrative privileges to install this script.\nPlease run: sudo ./install.sh"
 	exit 1
 fi
 
-ACTUAL_USER=$(sudo stat -f '%Su' ~)
+#-- Force OS X 'stat' in case of GNU version installed
+ACTUAL_USER=$(sudo /usr/bin/stat -f '%Su' ~)
 
 uninstallmode=false
 
@@ -20,27 +30,26 @@ while getopts ":u" opt; do
 		uninstallmode=true
       ;;
     \?)
-      echo "Invalid option: -$OPTARG" >&2
+      ${ECHO} "Invalid option: -$OPTARG" >&2
       ;;
   esac
 done
 
 
 if [ "$uninstallmode" = true ]; then
-	echo "Removing hooks..."
+	${ECHO} "Removing hooks..."
 	defaults delete com.apple.loginwindow LoginHook
 	defaults delete com.apple.loginwindow LogoutHook
 	
-	echo "Removing files..."
+	${ECHO} "Removing files..."
 	rm ~/.nobootsound_loginhook
 	rm ~/.nobootsound_logouthook
 	rm ~/.nobootsound_logoutvol
 	
 else
-	echo "Copying files..."
+	${ECHO} "Copying files..."
 	# Create file .nobootsound_logoutvol where the mute state is stored
-	sudo -u $ACTUAL_USER touch ~/.nobootsound_logoutvol
-	sudo -u $ACTUAL_USER echo "false" > ~/.nobootsound_logoutvol
+	sudo -u $ACTUAL_USER ${ECHO} "false" > ~/.nobootsound_logoutvol
 	
 	# Copy login and logout scripts and make them executable
 	sudo -u $ACTUAL_USER cp nobootsound_loginhook ~/.nobootsound_loginhook
@@ -48,10 +57,10 @@ else
 	sudo -u $ACTUAL_USER chmod +x ~/.nobootsound_loginhook
 	sudo -u $ACTUAL_USER chmod +x ~/.nobootsound_logouthook
 
-	echo "Registering hooks..."
+	${ECHO} "Registering hooks..."
 	# Register the scripts as login and logout hooks
 	defaults write com.apple.loginwindow LoginHook  ~/.nobootsound_loginhook
 	defaults write com.apple.loginwindow LogoutHook ~/.nobootsound_logouthook
 
-	echo "Done!"
+	${ECHO} "Done!"
 fi
